76987d5af37f386d8942c8db51df725cb21070ab
glennk4 <79699818+glennk4@users.noreply.github.com>
2021-08-06 09:17:46 +0100

    Initial commit

---
+++ Word Frequency/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Word_Frequency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.ListOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenButton.Location = new System.Drawing.Point(643, 363);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(170, 60);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Open File";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // ListOutput
+            // 
+            this.ListOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListOutput.FormattingEnabled = true;
+            this.ListOutput.ItemHeight = 16;
+            this.ListOutput.Location = new System.Drawing.Point(528, 89);
+            this.ListOutput.Name = "ListOutput";
+            this.ListOutput.Size = new System.Drawing.Size(401, 228);
+            this.ListOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 576);
+            this.Controls.Add(this.ListOutput);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "Word Frequency";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.ListBox ListOutput;
+    }
+}
+
---
+++ Word Frequency/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace Word_Frequency
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> words = new Dictionary<string,int>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            ListOutput.Items.Clear(); 
+
+            StreamReader inputFile;
+            openFileDialog1.InitialDirectory = @"C:\Users\";
+
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    List<string> original = new List<string>();
+                    char delim = ' ';
+                    string[] tokens;
+                    
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+
+                    while (!inputFile.EndOfStream)
+                    {
+                        string line = inputFile.ReadLine();
+                        tokens = line.Split(delim);
+
+                        foreach (string token in tokens)
+                        {
+                            original.Add(token); 
+                        }
+                    }
+
+                    inputFile.Close();
+
+                    PopulateDictionary(original); 
+                    DisplayResults();
+                }
+                else
+                {
+                    MessageBox.Show("Operation Cancelled");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); 
+            }
+        }
+
+
+        private void PopulateDictionary(List<string> original)
+        {
+            List<int> occurances = new List<int>();
+
+            //Count the occurances 
+            for (int i = 0; i < original.Count; i++)
+            {
+                int counter = 1;
+                for (int check = i + 1; check < original.Count; check++)
+                {
+                    if (original[i].ToLower().Equals(original[check].ToLower()))
+                    {
+                        counter++;
+                    }
+                }
+                occurances.Add(counter);
+            }
+
+            //Remove duplicates and add to dictionary
+            for (int i = 0; i < original.Count; i++)
+            {
+                for (int position = i + 1; position < original.Count; position++)
+                {
+                    if (original[i].ToLower().Equals(original[position].ToLower()))
+                    {
+                        original.RemoveAt(position);
+                        occurances.RemoveAt(position);
+                    }
+                }
+                words.Add(original[i], occurances[i]);
+            }
+  
+        }
+
+
+        private void DisplayResults()
+        {
+            foreach (KeyValuePair<string, int> element in words)
+            {
+                if (element.Value > 1)
+                {
+                    ListOutput.Items.Add("The word " + element.Key + " appears " + element.Value + " times"); 
+                }
+            }
+        }
+        
+    }
+}
